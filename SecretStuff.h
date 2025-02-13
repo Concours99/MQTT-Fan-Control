2826489f25fa7bc0028357e46a1d8cda097f6f9a
Concours99 <geiserw@gmail.com>
2025-02-12 20:27:44 -0500

    Initial commit
    The released files as of now.

---
+++ SecretStuff.h
@@ -0,0 +1,21 @@
+//###########################################################################
+//
+// Secret definitions
+//
+// This file contains definitions for Arduino code that I do not want stored
+// on GitHub with the code.
+//
+// Copyright (C) 2018-2023, Wayne Geiser.  All Rights Reserved.
+// email: geiserw@gmail.com
+
+#define SECRET_SSID "Bat Cave" // WiFi etwork name
+#define SECRET_PASS "huge46gas15"  // WiFi network password
+
+#define SECRET_MQTT_BROKER_IP_LOCAL "192.168.1.241" // MQTT broker on local 
+                                                    // network
+													
+#define SECRET_MQTT_USER "mqtt-user"   // MQTT user name
+#define SECRET_MQTT_PASS "SF4ABL6r6LY" // MQTT user's password
+#define SECRET_MQTT_PORT 1883
+#define SECRET_MQTT_USER "mqtt-user"
+#define SECRET_MQTT_PASS "SF4ABL6r6LY"
